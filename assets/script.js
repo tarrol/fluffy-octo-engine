9e3668938f974a29246d38c27b6e9bb916492a50
Andrey Skorodumov <quickandrey@gmail.com>
2022-12-04 16:51:59 -0800

    HTML basically done. Created the header banner, the search features, today's weather, and 5 day forecast cards

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '46903eec69982e0c4ee70b0657d599cb'
+
+
+
+
+
+// btn to save city in li
+// --> btn also saves in local storage
+// --> li also sends saved data to api to pull same place
+// btn to send request to api
+
+// convert city name into coordinates
+// unhide elements on page
