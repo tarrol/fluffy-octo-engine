721dc7c4a600252842ed0579e4b411d9361c9270
Andrey Skorodumov <quickandrey@gmail.com>
2022-12-04 20:56:33 -0800

    Created a search by zip code which then finds the weather and puts it on the page

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,26 @@
 var weatherURL = 'https://api.openweathermap.org';
 var weatherApiKey = '46903eec69982e0c4ee70b0657d599cb'
 
+var nameOnPage = document.getElementById('weatherLocation');
+var tempOnPage = document.getElementById('weatherTemp')
+var humidOnPage = document.getElementById('weatherHumid')
+var windOnPage = document.getElementById('weatherWindSpeed')
 
-
-var latitude = "";
-var longitude = "";
+var inputLatitude = "";
+var inputLongitude = "";
 var inputLocation = "San Diego";
-var zipCode = "92679"
+var zipCode = "92606"
 var countryCode = "US"
 
-var weatherSearch = `${weatherURL}/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherApiKey}`
+var outputTemperature
+var outputHumidity
+var outputWind
 
-var coordSearch = `${weatherURL}/geo/1.0/direct?q=${inputLocation}&limit=5&appid=${weatherApiKey}`
+var coordSearch = `${weatherURL}/geo/1.0/direct?q=${inputLocation}&limit=5&appid=${weatherApiKey}&units=imperial`
 
-var coordSearchZIP = `${weatherURL}/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=${weatherApiKey}`
+var coordSearchZIP = `${weatherURL}/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=${weatherApiKey}&units=imperial`
 
 
-
-function getWeather() {
-  fetch(weatherSearch)
-    .then(function (response))
-}
 
 
 function getCoordinates() {
@@ -30,9 +30,31 @@ function getCoordinates() {
     })
     .then(function (data) {
       console.log(data);
-      latitude = data.lat;
-      longitude = data.lon;
+      inputLatitude = data.lat;
+      inputLongitude = data.lon;
+    })
+    .then(function () {
+      console.log(inputLatitude);
+      console.log(inputLongitude);
       getWeather();
+    })
+}
+
+function getWeather() {
+  fetch(`${weatherURL}/data/2.5/weather?lat=${inputLatitude}&lon=${inputLongitude}&appid=${weatherApiKey}&units=imperial`)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data);
+      outputTemperature = data.main.temp;
+      outputHumidity = data.main.humidity;
+      outputWind = data.wind.speed
+      inputLocation = data.name
+      nameOnPage.innerText = inputLocation;
+      tempOnPage.innerText = `Temp: ${Math.floor(outputTemperature)}F`;
+      windOnPage.innerText = `Wind Speed: ${outputWind}MPH`;
+      humidOnPage.innerText = `Humidity: ${outputHumidity}%`;
     })
 }
 
